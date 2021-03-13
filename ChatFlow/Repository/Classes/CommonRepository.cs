1ada61ee3fdcc535ebd584c90ca9464d1d4d9861
Buzási Simon <buzasi.simon@gmail.com>
2021-03-13 11:08:26 +0100

    general abstract repository class added

---
+++ ChatFlow/Repository/Classes/CommonRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Classes
+{
+    public abstract class CommonRepository<T> : ICommonRepository<T> where T : class
+    {
+        protected DbContext context;
+
+        public CommonRepository(DbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(T item)
+        {
+            this.context.Set<T>().Add(item);
+            this.context.SaveChanges();
+        }
+
+        public void Delete(T item)
+        {
+            this.context.Set<T>().Remove(item);
+            this.context.SaveChanges();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            return this.context.Set<T>();
+        }
+
+        public abstract T GetOne(string id);
+
+        public void Save()
+        {
+            this.context.SaveChanges();
+        }
+
+        public abstract void Update(T updatedItem);
+    }
+}
