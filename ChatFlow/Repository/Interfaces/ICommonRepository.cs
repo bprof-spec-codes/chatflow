ca21c94502169addbbaad667b0913c6b8d80b6be
Buzási Simon <buzasi.simon@gmail.com>
2021-03-13 11:07:58 +0100

    general repository interface added

---
+++ ChatFlow/Repository/Interfaces/ICommonRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace Repository.Interfaces
+{
+    // general repo interface: CRUD + save(for methods in Logic if needed)
+    public interface ICommonRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        T GetOne(string id);
+        void Add(T item);
+        void Update(T updatedItem);
+        void Delete(T item);
+        void Save();
+    }
+}
