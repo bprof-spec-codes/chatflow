df2a358acf2d2c57a7339eb0080bdfc23b1c8e93
Boldi Bihari <boldibihari@gmail.com>
2021-03-15 13:36:33 +0100

    Messages done

--- ChatFlow/Logic/Classes/MessagesLogic.cs
+++ ChatFlow/Logic/Classes/MessagesLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Logic.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Logic.Classes
 {
-    class MessagesLogic
+    public class MessagesLogic : IMessagesLogic
     {
     }
 }
