f8663bf503b20e400516e8bed8814e958259cceb
Boldi Bihari <boldibihari@gmail.com>
2021-03-15 13:38:24 +0100

    Messages done

--- ChatFlow/Logic/Classes/MessagesLogic.cs
+++ ChatFlow/Logic/Classes/MessagesLogic.cs
@@ -1,4 +1,6 @@
 ﻿using Logic.Interfaces;
+using Models;
+using Repository.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,36 @@ namespace Logic.Classes
 {
     public class MessagesLogic : IMessagesLogic
     {
+        ICommonRepository<Messages> messagesRepository;
+
+        public MessagesLogic(ICommonRepository<Messages> messagesRepository)
+        {
+            this.messagesRepository = messagesRepository;
+        }
+
+        public void AddMessages(Messages messages)
+        {
+            messagesRepository.Add(messages);
+        }
+
+        public void DeleteMessages(Messages messages)
+        {
+            messagesRepository.Delete(messages);
+        }
+
+        public IQueryable<Messages> GetAllMessages()
+        {
+            return messagesRepository.GetAll();
+        }
+
+        public Messages GetOneMessages(string id)
+        {
+            return messagesRepository.GetOne(id);
+        }
+
+        public void UpdateMessages(Messages updatedItem)
+        {
+            messagesRepository.Update(updatedItem);
+        }
     }
 }
