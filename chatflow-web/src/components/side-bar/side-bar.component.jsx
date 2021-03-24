52d3363c848e359df5e52e431ab77a5941f31bdc
tamas-lengyel <lengyeltamas97@gmail.com>
2021-03-24 12:31:26 +0100

    Base component done

---
+++ chatflow-web/src/components/side-bar/side-bar.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "antd/dist/antd.css";
+import "./side-bar.styles.css";
+import { Menu, Layout} from "antd";
+
+const { Sider } = Layout;
+
+export const Sidebar = (props) => (
+    <div>
+
+    </div>
+);
