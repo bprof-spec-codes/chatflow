e49603e4e517a5b11e87c162b43c6446da44e350
tamas-lengyel <lengyeltamas97@gmail.com>
2021-03-24 12:50:44 +0100

    Sidebar component done

--- chatflow-web/src/components/side-bar/side-bar.component.jsx
+++ chatflow-web/src/components/side-bar/side-bar.component.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React from "react";
 import "antd/dist/antd.css";
 import "./side-bar.styles.css";
-import { Menu, Layout} from "antd";
+import { Menu, Layout } from "antd";
+import { Link } from "react-router-dom";
 
 const { Sider } = Layout;
 
 export const Sidebar = (props) => (
-    <div>
-
-    </div>
+  <Sider className="side-bar">
+    <div className="logo">Title</div>
+    <Menu theme="dark">
+      <Menu.Item key="1">
+        <Link to="/channel1">Channel 1</Link>
+      </Menu.Item>
+      <Menu.Item key="2">
+        <Link to="/channel2">Channel 2</Link>
+      </Menu.Item>
+      <Menu.Item key="3">
+        <Link to="/channel3">Channel 3</Link>
+      </Menu.Item>
+    </Menu>
+  </Sider>
 );
