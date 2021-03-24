4a8a8e9a519119defbae9376095f4782c7f74760
tamas-lengyel <lengyeltamas97@gmail.com>
2021-03-24 13:46:45 +0100

    Header component inital setup

---
+++ chatflow-web/src/components/header/header.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "antd/dist/antd.css";
+import "./header.styles.css";
+import { Layout } from "antd";
+
+const { Header } = Layout;
+
+export const Header = (props) => (
+    <div>
+
+    </div>
+);
