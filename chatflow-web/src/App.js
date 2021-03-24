03419b42ef8257a3d66f8d13c9037cbd3a7d78b2
tamas-lengyel <lengyeltamas97@gmail.com>
2021-03-24 14:22:14 +0100

    Header component added to App

--- chatflow-web/src/App.js
+++ chatflow-web/src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import { Layout } from "antd";
 import { Sidebar } from "./components/side-bar/side-bar.component";
+import { Headerr } from "./components/header/header.component";
 
 function App() {
   return (
     <div className="App">
       <Layout style={{ minHeight: "100vh", width: "100vw" }}>
         <Sidebar></Sidebar>
+        <Layout className="site-layout">
+          <Headerr></Headerr>
+        </Layout>
       </Layout>
     </div>
   );
