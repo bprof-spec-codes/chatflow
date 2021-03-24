517c03960bc70e5e0d154b8f711f587b984c1e98
tamas-lengyel <lengyeltamas97@gmail.com>
2021-03-24 13:20:05 +0100

    Added the component to App

--- chatflow-web/src/App.js
+++ chatflow-web/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Layout } from "antd";
+import { Sidebar } from "./components/side-bar/side-bar.component";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout style={{ minHeight: "100vh", width: "100vw" }}>
+        <Sidebar></Sidebar>
+      </Layout>
     </div>
   );
 }
--- chatflow-web/src/components/side-bar/side-bar.component.jsx
+++ chatflow-web/src/components/side-bar/side-bar.component.jsx
@@ -8,16 +8,19 @@ const { Sider } = Layout;
 
 export const Sidebar = (props) => (
   <Sider className="side-bar">
-    <div className="logo">Title</div>
+    <div className="logo">ChatFlow</div>
     <Menu theme="dark">
       <Menu.Item key="1">
-        <Link to="/channel1">Channel 1</Link>
+        {/* <Link to="/channel1">Channel 1</Link> */}
+        Channel 1
       </Menu.Item>
       <Menu.Item key="2">
-        <Link to="/channel2">Channel 2</Link>
+        {/* <Link to="/channel2">Channel 2</Link> */}
+        Channel 2
       </Menu.Item>
       <Menu.Item key="3">
-        <Link to="/channel3">Channel 3</Link>
+        {/* <Link to="/channel3">Channel 3</Link> */}
+        Channel 3
       </Menu.Item>
     </Menu>
   </Sider>
