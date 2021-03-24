1db1b394193a6bc56173c0dc1150c6a2f7175b72
tamas-lengyel <lengyeltamas97@gmail.com>
2021-03-24 17:10:36 +0100

    Inserted a couple Comment components for a better layout view

--- chatflow-web/src/components/content-container/content-container.component.jsx
+++ chatflow-web/src/components/content-container/content-container.component.jsx
@@ -2,11 +2,38 @@ import React from "react";
 import "antd/dist/antd.css";
 import "./content-container.styles.css";
 import { Layout } from "antd";
+import { Demo } from "../comment/comment.component";
 
 const { Content } = Layout;
 
 export const ContentContainer = (props) => (
   <Content className="content-container">
-    <div className="site-layout-background">{props.messages}</div>
+    <div className="box">
+      <Demo></Demo>
+    </div>
+    <div className="box">
+      <Demo></Demo>
+    </div>
+    <div className="box">
+      <Demo></Demo>
+    </div>
+    <div className="box">
+      <Demo></Demo>
+    </div>
+    <div className="box">
+      <Demo></Demo>
+    </div>
+    <div className="box">
+      <Demo></Demo>
+    </div>
+    <div className="box">
+      <Demo></Demo>
+    </div>
+    <div className="box">
+      <Demo></Demo>
+    </div>
+    <div className="box">
+      <Demo></Demo>
+    </div>
   </Content>
 );
