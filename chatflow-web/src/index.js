403f09f317494f9b057d7bda3227214e4a6c7719
Szabó Dárius <szabo.dariusz@gmail.com>
2021-03-29 13:41:56 +0200

    login component created

--- chatflow-web/src/index.js
+++ chatflow-web/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { NormalLoginForm } from './components/login/login.component';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    
+    <NormalLoginForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
