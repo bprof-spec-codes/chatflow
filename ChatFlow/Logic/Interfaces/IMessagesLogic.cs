bbfc84ad958572a75b6d5ebf3d361919b67a8e17
Boldi Bihari <boldibihari@gmail.com>
2021-04-03 14:33:07 +0200

    IMessagesLogic done

--- ChatFlow/Logic/Interfaces/IMessagesLogic.cs
+++ ChatFlow/Logic/Interfaces/IMessagesLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,10 @@ namespace Logic.Interfaces
 {
     interface IMessagesLogic
     {
-        //TODO
+        void AddMessage(Messages messages);
+        void DeleteMessage(Messages messages);
+        IQueryable<Messages> GetAllMessage();
+        Messages GetOneMessage(string idMessages);
+        void UpdateMessage(Messages updatedMessages);
     }
 }
