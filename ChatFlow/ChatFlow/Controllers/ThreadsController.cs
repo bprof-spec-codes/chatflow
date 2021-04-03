20e8f6782cd7fc415f15faedace32ddb2c17b5a9
Boldi Bihari <boldibihari@gmail.com>
2021-04-03 16:16:41 +0200

    controllers done

---
+++ ChatFlow/ChatFlow/Controllers/ThreadsController.cs
@@ -0,0 +1,52 @@
+﻿using Logic.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatFlow.Controllers
+{
+    [ApiController]
+    [Route("{controller}")]
+    public class ThreadsController : ControllerBase
+    {
+        IThreadsLogic threadsLogic;
+
+        public ThreadsController(IThreadsLogic threadsLogic)
+        {
+            this.threadsLogic = threadsLogic;
+        }
+
+        [HttpPost]
+        public void AddThread([FromBody] Threads threads)
+        {
+            threadsLogic.AddThread(threads);
+        }
+
+        [HttpDelete]
+        public void DeleteThread([FromBody] Threads threads)
+        {
+            threadsLogic.DeleteThread(threads);
+        }
+
+        [HttpGet]
+        public IQueryable<Threads> GetAllThread()
+        {
+            return threadsLogic.GetAllThread();
+        }
+
+        [HttpGet("{idThreads}")]
+        public Threads GetOneThread(string idThreads)
+        {
+            return threadsLogic.GetOneThread(idThreads);
+        }
+
+        [HttpPut]
+        public void UpdateThread([FromBody] Threads updatedThreads)
+        {
+            threadsLogic.UpdateThread(updatedThreads);
+        }
+    }
+}
