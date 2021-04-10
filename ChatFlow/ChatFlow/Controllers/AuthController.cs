60179da685f617d58d1fa191bf2325273ee8e25b
Buzási Simon <buzasi.simon@gmail.com>
2021-04-10 18:24:52 +0200

    create controller for user management

---
+++ ChatFlow/ChatFlow/Controllers/AuthController.cs
@@ -0,0 +1,65 @@
+﻿using Logic.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatFlow.Controllers
+{
+    [ApiController]
+    [Route("{controller}")]
+    public class AuthController : ControllerBase
+    {
+        IAuthLogic logic;
+        public AuthController(IAuthLogic _logic)
+        {
+            this.logic = _logic;
+        }
+
+        [HttpPost]
+        public async Task<string> AddUser([FromBody] User user)
+        {
+            return await this.logic.CreateUser(user);
+        }
+
+        [HttpGet("{userid}")]
+        public async Task<User> GetUser(string userid)
+        {
+            return await this.logic.GetUser(userid);
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            return this.logic.GetAllUser();
+        }
+
+        [HttpDelete("{userid}")]
+        public async Task DeleteUser(string userid)
+        {
+            await this.logic.DeleteUser(userid);
+        }
+
+        [HttpPut]
+        public async Task UpdateUser([FromBody] User user)
+        {
+            await this.logic.UpdateUser(user);
+        }
+
+        [HttpPatch]
+        public async Task<ActionResult> Login([FromBody] LoginViewModel model)
+        {
+            try
+            {
+                return Ok(await this.logic.LoginUser(model));
+            }
+            catch (ArgumentException ex)
+            {
+
+                return BadRequest(new { Error = ex.Message });
+            }
+        }
+    }
+}
