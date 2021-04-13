b5ead55675e9e91affe2f1f73676728861d72124
Szabó Dárius <szabo.dariusz@gmail.com>
2021-04-13 12:21:16 +0200

    route to admin-ui created

--- chatflow-web/src/App.js
+++ chatflow-web/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { NormalLoginForm } from "./components/login/login.component";
 import { Home } from "./components/home/home.component";
+import AdminUI from "./components/admin-components/admin-ui/admin-ui.component";
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
         <Switch>
           <Route path="/login">
             <NormalLoginForm />
+          </Route>
+          <Route path="/admin">
+            <AdminUI />
           </Route>
           <Route path="/rooms/:id">
             <Home />
