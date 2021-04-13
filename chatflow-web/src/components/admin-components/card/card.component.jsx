f9e89cb0cfc8272359fcb46ed73c47acc3f63f51
Szabó Dárius <szabo.dariusz@gmail.com>
2021-04-13 19:23:22 +0200

    cards upgraded with buttons

--- chatflow-web/src/components/admin-components/card/card.component.jsx
+++ chatflow-web/src/components/admin-components/card/card.component.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './card.styles.css'
 
+import { Button } from 'antd';
+import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
+
 export const Card = props => (
     <div className='card-container'>
         <div className='avatar'>
@@ -12,6 +15,10 @@ export const Card = props => (
         <div className='personal-data'>
             <h2>{props.member.name}</h2>
             <p>{props.member.email}</p>
+        </div>
+        <div className='tools'>
+            <Button shape='round' icon={<DeleteOutlined />}></Button>
+            <Button shape='round' icon={<EditOutlined />}></Button>
         </div>
     </div>
 
