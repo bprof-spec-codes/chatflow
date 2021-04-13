963cbf23c525e7fbdd3ef759934a19f5186c18c1
Szabó Dárius <szabo.dariusz@gmail.com>
2021-04-13 15:19:28 +0200

    user cards style modified

--- chatflow-web/src/components/admin-components/card/card.component.jsx
+++ chatflow-web/src/components/admin-components/card/card.component.jsx
@@ -3,11 +3,17 @@ import './card.styles.css'
 
 export const Card = props => (
     <div className='card-container'>
-        <img
-            alt='monster'
-            src={`https://robohash.org/${props.member.id}?set=set3&size=180x180`}>
-        </img>
-        <h2>{props.member.name}</h2>
-        <p>{props.member.email}</p>
+        <div className='avatar'>
+            <img
+                alt='monster'
+                src={`https://robohash.org/${props.member.id}?set=set3&size=180x180`}>
+            </img>
+        </div>
+        <div className='personal-data'>
+            <h2>{props.member.name}</h2>
+            <p>{props.member.email}</p>
+        </div>
     </div>
+
+
 )
