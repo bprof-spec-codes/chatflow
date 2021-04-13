7cf871f3e2ddfa2d6903702f04ea548009b9efaf
Szabó Dárius <szabo.dariusz@gmail.com>
2021-04-13 15:23:35 +0200

    admin-ui component added

--- chatflow-web/src/components/admin-components/admin-ui/admin-ui.component.jsx
+++ chatflow-web/src/components/admin-components/admin-ui/admin-ui.component.jsx
@@ -1,9 +1,11 @@
 import { CardList } from '../card-list/card-list.component';
 import React, { Component } from 'react';
 
+import { PopUp } from '../pop-up/pop-up.component'
+
 class AdminUI extends Component {
   constructor() {
-    super(); 
+    super();
 
     this.state = {
       members: [],
@@ -11,8 +13,7 @@ class AdminUI extends Component {
     };
   }
 
-  componentDidMount()
-  {
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ members: users }));
@@ -23,13 +24,15 @@ class AdminUI extends Component {
       <div className="AdminUI">
         <input
           type='search'
-          placeholder='search user' 
+          placeholder='search user'
           onChange={e => {
             this.setState({ searchField: e.target.value }, () => console.log(this.state));
           }} />
-          <CardList
+
+        <CardList
           members={this.state.members}>
-        </CardList>        
+        </CardList>
+        <PopUp/>
       </div >
     );
   }
