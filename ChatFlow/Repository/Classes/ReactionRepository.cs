0816220210cbae950c006be1ad4459711e055061
Boldi Bihari <boldibihari@gmail.com>
2021-04-18 21:46:48 +0200

    ReactionRepository created

---
+++ ChatFlow/Repository/Classes/ReactionRepository.cs
@@ -0,0 +1,53 @@
+﻿using Data;
+using Models;
+using Repository.Classes;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ReactionRepository : IReactionRepository
+    {
+        ChatFlowContext context;
+
+        public ReactionRepository(ChatFlowContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(Reaction reaction)
+        {
+            this.context.Set<Reaction>().Add(reaction);
+            this.context.SaveChanges();
+        }
+
+        public void Delete(Reaction reaction)
+        {
+            this.context.Set<Reaction>().Remove(reaction);
+            this.context.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            Delete(GetOne(id));
+
+        }
+
+        public Reaction GetOne(string id)
+        {
+            return this.context.Set<Reaction>().SingleOrDefault(x => x.ReactionID == id);
+        }
+
+        public void Update(string id, ReactionType type)
+        {
+            Reaction reaction = this.context.Set<Reaction>().SingleOrDefault(x => x.ReactionID == id);
+            reaction.ReactionType = type;
+
+            this.context.SaveChanges();
+        }
+    }
+}
