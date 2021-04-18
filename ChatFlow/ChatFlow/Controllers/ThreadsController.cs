8640014d28c3db4eb7aaef864d281dbb18d15d92
Boldi Bihari <boldibihari@gmail.com>
2021-04-18 21:54:16 +0200

    ThreadsController modified

--- ChatFlow/ChatFlow/Controllers/ThreadsController.cs
+++ ChatFlow/ChatFlow/Controllers/ThreadsController.cs
@@ -33,10 +33,17 @@ namespace ChatFlow.Controllers
             threadsLogic.DeleteThread(threads);
         }
 
+        //only for testing
         [HttpGet]
         public IQueryable<Threads> GetAllThread()
         {
             return threadsLogic.GetAllThread();
+        }
+
+        [HttpGet("Room/{idRoom}")]
+        public IQueryable<Threads> GetAllThreadFromRoom(string idRoom)
+        {
+            return threadsLogic.GetAllThreadFromRoom(idRoom);
         }
 
         [HttpGet("{idThreads}")]
@@ -49,6 +56,55 @@ namespace ChatFlow.Controllers
         public void UpdateThread([FromBody] Threads updatedThreads)
         {
             threadsLogic.UpdateThread(updatedThreads);
+        }
+
+        [HttpGet("Pinned/{idRoom}")]
+        public IQueryable<Threads> GetAllPinnedThread(string idRoom)
+        {
+            return threadsLogic.GetAllPinnedThread(idRoom);
+        }
+
+        [HttpPut("Pin/{idThreads}")]
+        public void PinThread(string idThreads)
+        {
+            threadsLogic.PinThread(idThreads);
+        }
+
+        [HttpPut("DeletePin/{idThreads}")]
+        public void DeletePinThread(string idThreads)
+        {
+            threadsLogic.DeletePinThread(idThreads);
+        }
+
+        [HttpPost("AddReaction/{idThreads}/{reactionNumber}")]
+        public void AddReactionToThread(string idThreads, int reactionNumber)
+        {
+            ReactionType reactionType = (ReactionType)reactionNumber;
+            Reaction reaction = new Reaction();
+            reaction.ThreadID = idThreads;
+            reaction.ReactionType = reactionType;
+
+            this.threadsLogic.AddReactionToThread(idThreads, reaction);
+        }
+
+        [HttpDelete("DeleteReaction/{idThreads}")]
+        public void DeleteReactionFromThread(string idThreads)
+        {
+            this.threadsLogic.DeleteReactionFromThread(idThreads);
+        }
+
+        [HttpPut("UpdateReaction/{idThreads}/{reactionNumber}")]
+        public void UpdateReactionOnThread(string idThreads, int reactionNumber)
+        {
+            if (reactionNumber <= 3 && reactionNumber > 0)
+            {
+                ReactionType reactionType = (ReactionType)reactionNumber;
+                this.threadsLogic.UpdateReactionOnThread(idThreads, reactionType);
+            }
+            else
+            {
+                throw new Exception("Invalid number!");
+            }
         }
     }
 }
