43dec6d0d7d462186c68b728d20e193643072b58
Szabó Dárius <szabo.dariusz@gmail.com>
2021-05-11 18:45:21 +0200

    fixed every user api call

--- chatflow-web/src/components/admin-components/admin-layout/admin-layout.component.jsx
+++ chatflow-web/src/components/admin-components/admin-layout/admin-layout.component.jsx
@@ -5,13 +5,10 @@ import { Layout, Menu } from 'antd';
 import { TeamOutlined, UserOutlined } from '@ant-design/icons';
 import { CardList } from '../card-list/card-list.component';
 import { SearchBox } from '../search-box/search-box.component';
-import axios from 'axios'
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
-const api = axios.create({
-    baseURL: 'https://localhost:5000'
-})
+const axios = require("axios");
 
 class AdminLayout extends Component {
     state = {
@@ -32,75 +29,61 @@ class AdminLayout extends Component {
             searchUser: '',
             token: '',
         };
-        
-
-
-
     }
 
     componentDidMount() {
-        /*fetch('https://localhost:44325/auth/')
-            .then(response => response.json())
-            .then(users => this.setState({ members: users }));*/
-        api.get(`/auth/login`,
-            {
-                userName : 'admin',
-                password : 'admin'
-            })
-            .then(response => console.log(response)).catch((error) => {
-                console.log(error.message);
-              });
-            /*.then(getToken => this.setState({ token: getToken }))
-            .then(
-                api.get('/auth',{ 'Authorization': `Bearer ${this.state.token}` })
-                    .then(response => response.json())
-                    .then(users => this.setState({ members: users })
-                    )
-            )*/
-    }
 
-    render() {
-        const { members, searchUser } = this.state;
-        const filteredMembers = members.filter(member =>
-            member.userName.toLowerCase().includes(searchUser.toLowerCase())
+            const minTime = new Promise((resolve) => setTimeout(resolve, 200));
+            const req = axios.get(`/auth`);
+      
+            Promise.all([minTime, req]).then((values) => {
+              const reqData = values[1];
+              this.setState({members:reqData.data});}
         );
-        const { collapsed } = this.state;
-        return (
-            <Layout style={{ minHeight: '100vh' }}>
+  }
 
-                <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse} className='sider'>
-                    <div className="logo" />
-                    <Menu defaultSelectedKeys={['1']} mode="inline" className='sider'>
-                        <SubMenu key="sub1" icon={<UserOutlined />} title="User Options">
-                            <Menu.Item key="1">Add</Menu.Item>
-                            <Menu.Item key="2">
-                                <SearchBox
-                                    placeholder='Search User'
-                                    handleChange={e => this.setState({ searchUser: e.target.value })}
-                                />
-                            </Menu.Item>
-                        </SubMenu>
-                        <SubMenu key="sub2" icon={<TeamOutlined />} title="Group Options">
-                            <Menu.Item key="3">Add</Menu.Item>
-                        </SubMenu>
-                    </Menu>
-                </Sider>
+render() {
+    const { members, searchUser } = this.state;
+    const filteredMembers = members.filter(member =>
+        member.userName.toLowerCase().includes(searchUser.toLowerCase())
+    );
+    const { collapsed } = this.state;
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
 
-                <Layout className="site-layout-user">
-                    <Header className="site-layout-background" />
-                    <Content style={{ margin: '0 16px' }} className='content-container'>
-                        <div className='search'>
-                            <CardList
-                                members={filteredMembers}>
-                            </CardList>
-                        </div>
-                    </Content>
-                    <Footer style={{ textAlign: 'center' }}>Created by Team Chatflow</Footer>
-                </Layout>
+            <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse} className='sider'>
+                <div className="logo" />
+                <Menu defaultSelectedKeys={['1']} mode="inline" className='sider'>
+                    <SubMenu key="sub1" icon={<UserOutlined />} title="User Options">
+                        <Menu.Item key="1">Add</Menu.Item>
+                        <Menu.Item key="2">
+                            <SearchBox
+                                placeholder='Search User'
+                                handleChange={e => this.setState({ searchUser: e.target.value })}
+                            />
+                        </Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="sub2" icon={<TeamOutlined />} title="Group Options">
+                        <Menu.Item key="3">Add</Menu.Item>
+                    </SubMenu>
+                </Menu>
+            </Sider>
+
+            <Layout className="site-layout-user">
+                <Header className="site-layout-background" />
+                <Content style={{ margin: '0 16px' }} className='content-container'>
+                    <div className='search'>
+                        <CardList
+                            members={filteredMembers}>
+                        </CardList>
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>Created by Team Chatflow</Footer>
             </Layout>
-            
+        </Layout>
 
-        );
-    }
+
+    );
+}
 }
 export default AdminLayout;
