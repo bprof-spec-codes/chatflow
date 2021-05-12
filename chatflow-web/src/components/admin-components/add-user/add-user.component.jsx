57a34c7191ef68e8fd6c918c46804c18d18e8655
Szabó Dárius <szabo.dariusz@gmail.com>
2021-05-12 10:26:40 +0200

    Add User function Modified

--- chatflow-web/src/components/admin-components/add-user/add-user.component.jsx
+++ chatflow-web/src/components/admin-components/add-user/add-user.component.jsx
@@ -1,35 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import 'antd/dist/antd.css';
 import './add-user.style.css'
-import { Form, Input, Button, Radio, Select, Cascader, DatePicker, InputNumber, TreeSelect, Switch } from 'antd';
+import { Form, Button, Select } from 'antd';
 
 export const AddUser = () => {
+    const [users, setUser] = useState(null);
+    const [rooms, setRoom] = useState(null);
+    const axios = require("axios");
+    const onFinish = (values) => {
+        axios
+            .post("/auth/{user.id}/{room.roomID}")
+    };
+
+
+    useEffect(() => {
+
+        const minTime = new Promise((resolve) => setTimeout(resolve, 200));
+        const req = axios.get(`/auth`);
+
+        Promise.all([minTime, req]).then((values) => {
+            const reqData = values[1];
+            console.log(reqData.data);
+            setUser(reqData.data);
+        }
+        );
+    }, [axios, setUser]);
+
+    useEffect(() => {
+
+        const minTime2 = new Promise((resolve) => setTimeout(resolve, 300));
+        const req2 = axios.get(`/room`);
+
+        Promise.all([minTime2, req2]).then((values) => {
+            const reqData2 = values[1];
+            console.log(reqData2.data);
+            setRoom(reqData2.data);
+        }
+        );
+    }, [axios, setRoom]);
+
+
     return (
         <>
-        <div className="logo-login">ChatFlow</div>
+            <div className="logo-login">ChatFlow</div>
             <div className='form-container'>
                 <div className='form-container2'>
-                    <Form>
-                        <Form.Item label="Name" >
-                            <Input className='form-item' />
-                        </Form.Item>
-                        <Form.Item label="Password">
-                            <Input className='form-item' />
-                        </Form.Item>
-                        <Form.Item label="Select Rank">
+                    <Form onFinish={onFinish}>
+                        <Form.Item label="Select User">
                             <Select>
-                                <Select.Option value="demo">Admin</Select.Option>
-                                <Select.Option value="demo">User</Select.Option>
+                                {users?.map(user => (
+                                    <Select.Option value={user.id}>{user.userName}</Select.Option>
+                                ))}
+                            </Select>
+                        </Form.Item>
+                        <Form.Item label="Select Room">
+                            <Select>
+                                {rooms?.map(room => (
+                                    <Select.Option value={room.roomID}>{room.roomName}</Select.Option>
+                                ))}
                             </Select>
                         </Form.Item>
                         <Form.Item>
-                            <Button type="primary" htmlType="submit" className='add-form-button'>Add User</Button>
+                            <Button onClick='' type="primary" htmlType="submit" className='add-form-button'>Add User</Button>
                         </Form.Item>
                     </Form>
                 </div>
-
             </div>
-
         </>
     );
 };
